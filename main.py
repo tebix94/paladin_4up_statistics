d3d8a5876eee7b73f0e8bc2a6a124346f3ba1d40
auto <esteban.torres.perez.1994@gmail.com>
2025-11-22 13:26:33 -0800

    Adding CT updated bits from PLCs, although it has a solution to look for rising edges for each status bit, it has not been fully tested yet

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 import sys
 import time
 import threading
-import pandas
+import pandas as pd
 from packages.plc_socket import kv_plc_tcp_socket
 
-# PLC names list
+# PLC names
 plc_names = ('M1 Gasket',
              'M2 PHD',
              'M3 Male IMLA assy',
@@ -16,7 +16,7 @@ plc_names = ('M1 Gasket',
              'M8 Short/Code',
              )
 
-# Instance line 1 PLC's
+# Instance all PLC's
 plc_list = []
 
 plc_list.append(kv_plc_tcp_socket('192.168.0.10', 8501, 3)) 
@@ -28,11 +28,13 @@ plc_list.append(kv_plc_tcp_socket('192.168.0.60', 8501, 3))
 plc_list.append(kv_plc_tcp_socket('192.168.0.70', 8501, 3))
 plc_list.append(kv_plc_tcp_socket('192.168.0.80', 8501, 3))
 
-
 # Create CT list, 
-# The update bit list stores the flag from the PLC program that will tell us if CT has been changed
+# The updated bit list stores the flag from the PLC program that will tell us if CT has been changed
 ct_list = [0.] * len(plc_list)
-update_bit_list = [False] * len(plc_list)
+updated_ct_bit_list = [[False, False] for _ in range(len(plc_list))] # <- [[New value, Last value]]
+
+# Create Pandas series for statistics
+ct_series_list = [pd.Series() for _ in range(len(plc_list))]
 
 # Thread functions
 def t_get_ct(plc, index):
@@ -41,35 +43,41 @@ def t_get_ct(plc, index):
     except Exception as e:
         print(f'Exception at t_get_ct(plc, index): {e} at {plc_names[index]}')
 
-def t_get_update_bit(plc, index):
+def t_get_updated_bit(plc, index):
     try:
-        update_bit_list[index] = plc.read('R', 4002)
+        # Shift last value
+        updated_ct_bit_list[index][1] = updated_ct_bit_list[index][0]
+        # Updated new value
+        updated_ct_bit_list[index][0] = plc.read('MR', 5912)
     except Exception as e:
-        print(f'Exception at t_get_update_bit(plc, index): {e} at {plc_names[index]}')
+        print(f'Exception at t_get_updated_bit(plc, index): {e} at {plc_names[index]}')
 
 try:
     # Main loop
-
     while (True):
 
         threads = []
 
-        for idx, plc in enumerate(plc_list):
-            t1 = threading.Thread(target=t_get_ct, args=(plc, idx))
+        # Fetch PLC's data
+        for index, plc in enumerate(plc_list):
+            t1 = threading.Thread(target=t_get_ct, args=(plc, index))
             t1.start()
             threads.append(t1)
-            t2 = threading.Thread(target=t_get_update_bit, args=(plc, idx))
+            t2 = threading.Thread(target=t_get_updated_bit, args=(plc, index))
             t2.start()
             threads.append(t2)
         for t in threads:
             t.join()
 
+        # Updated series if needed
+
+        # Prompt values
         os.system('cls' if os.name == 'nt' else 'clear')
         sys.stdout.write('\033[H')  # Move cursor to top left
-        for name, ct, update_bit in zip(plc_names, ct_list, update_bit_list):
+        for i in range(len(plc_names)):
             sys.stdout.write('\033[2K\r')  # Clear line and move to start
             sys.stdout.write(
-                f'{name}: {ct} seconds, {update_bit}'
+                f'{plc_names[i]}: {ct_list[i]} seconds, [{updated_ct_bit_list[i][0]}, {updated_ct_bit_list[i][1]}]'
             )
             sys.stdout.write('\n')  # Move to next line
         sys.stdout.flush()
