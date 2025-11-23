ab0da809155357a542764feb1ebee9696eb006d1
auto <esteban.torres.perez.1994@gmail.com>
2025-11-22 17:26:52 -0800

    main.py has been modified to prevent script from not catching all ct values produced by each plc, although it does not completely prevent it, this version drastically reduces data lost. Data is still being lost since PLC and Python script are not still synchronized. The modification consists on moving the ct updated status bit from thread 2 into thread 1 leaving a single thread that sequentally read ct time and read ct time status bit. Additionally I am using Pandas to export ct data as a dataframe into XLSX file, the data quantity can be set at the beginning of the script by assigning the number of samples to the SAMPLE_SIZE constant.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 import sys
-import time
 import threading
 import pandas as pd
 from packages.plc_socket import kv_plc_tcp_socket
 
-# PLC names
-plc_names = ('M1 Gasket',
+# Constants
+SAMPLE_SIZE = 10
+FILENAME = 'Paladin_CT_sample.xlsx'
+PLC_NAMES = ('M1 Gasket',
              'M2 PHD',
              'M3 Male IMLA assy',
              'M4 Female IMLA assy',
@@ -30,27 +31,23 @@ plc_list.append(kv_plc_tcp_socket('192.168.0.80', 8501, 3))
 
 # Create CT list, 
 # The updated bit list stores the flag from the PLC program that will tell us if CT has been changed
-ct_list = [0.] * len(plc_list)
-updated_ct_bit_list = [[False, False] for _ in range(len(plc_list))] # <- [[New value, Last value]]
+ct_values_list = [0.] * len(plc_list)
 
-# Create Pandas series for statistics
-ct_series_list = [pd.Series() for _ in range(len(plc_list))]
+# The following list keep tracks of a PLC bit that presents a rising edge when the CT has been updated
+# Each element is for each PLC
+current_ct_bit_list = [False] * len(plc_list)
+last_ct_bit_list = [False] * len(plc_list)
+
+# Create temporary list o lists to be converted to Pandas dataframe after collecting sufficient data
+ct_temp_lists = [[] for _ in range(len(plc_list))]
 
 # Thread functions
 def t_get_ct(plc, index):
     try:
-        ct_list[index] = float(plc.read('DM', 88)) / 10.
+        ct_values_list[index] = float(plc.read('DM', 88)) / 10.
+        current_ct_bit_list[index] = plc.read('MR', 5912)
     except Exception as e:
-        print(f'Exception at t_get_ct(plc, index): {e} at {plc_names[index]}')
-
-def t_get_updated_bit(plc, index):
-    try:
-        # Shift last value
-        updated_ct_bit_list[index][1] = updated_ct_bit_list[index][0]
-        # Updated new value
-        updated_ct_bit_list[index][0] = plc.read('MR', 5912)
-    except Exception as e:
-        print(f'Exception at t_get_updated_bit(plc, index): {e} at {plc_names[index]}')
+        print(f'Exception at t_get_ct(plc, index): {e} at {PLC_NAMES[index]}')
 
 try:
     # Main loop
@@ -60,31 +57,42 @@ try:
 
         # Fetch PLC's data
         for index, plc in enumerate(plc_list):
-            t1 = threading.Thread(target=t_get_ct, args=(plc, index))
-            t1.start()
-            threads.append(t1)
-            t2 = threading.Thread(target=t_get_updated_bit, args=(plc, index))
-            t2.start()
-            threads.append(t2)
+            t = threading.Thread(target=t_get_ct, args=(plc, index))
+            t.start()
+            threads.append(t)
         for t in threads:
             t.join()
 
-        # Updated series if needed
+        # Update cycle time temporary list of lists
+        for index in range(len(ct_values_list)):
+            # Look for a rising edge from the PLC's ct status updated bits
+            if current_ct_bit_list[index] == True and last_ct_bit_list[index] == False:
+                ct_temp_lists[index].append(ct_values_list[index])
 
-        # Prompt values
+            # Keep shifting each ct updated status bit value
+            last_ct_bit_list[index] = current_ct_bit_list[index]
+
+        # Print in terminal
         os.system('cls' if os.name == 'nt' else 'clear')
         sys.stdout.write('\033[H')  # Move cursor to top left
-        for i in range(len(plc_names)):
+        for i in range(len(PLC_NAMES)):
             sys.stdout.write('\033[2K\r')  # Clear line and move to start
-            sys.stdout.write(
-                f'{plc_names[i]}: {ct_list[i]} seconds, [{updated_ct_bit_list[i][0]}, {updated_ct_bit_list[i][1]}]'
-            )
+            sys.stdout.write(f'{PLC_NAMES[i]}: {ct_values_list[i]} seconds')
+            sys.stdout.write(f'{ct_temp_lists[i]}')
             sys.stdout.write('\n')  # Move to next line
         sys.stdout.flush()
 
-        time.sleep(0.25)
+        # Convert temporary lists to Pandas dataframe
+        if len(ct_temp_lists[-1]) >= SAMPLE_SIZE:
+            df = pd.DataFrame(ct_temp_lists).T
+            df.columns = PLC_NAMES
+            with pd.ExcelWriter(FILENAME) as writer:
+                df.to_excel(writer, sheet_name='CT', index=False)
+            print(f'\n--- Successfully exported {SAMPLE_SIZE} samples to {FILENAME} ---')
+
+            # Clean lists values to start a new sample
+            ct_temp_lists = [[] for _ in range(len(plc_list))]
 
 except KeyboardInterrupt:
     print("\nProgram terminated by user (Ctrl+C). Exiting gracefully.")
-    # Add any necessary cleanup code here, like closing PLC connections
-    sys.exit(0) # Exit the program
+    sys.exit(0)
